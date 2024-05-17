e3651b311ad3aaf5a411a96862dbe0fdfc3885ff
Lucasch1 <lucasch2011@gmail.com>
2024-05-17 17:56:55 -0300

    image

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -59,39 +59,29 @@ const AboutSection = () => {
     return (
         <section className="text-white" id="about">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about-image.png" width={500} height={500} />
+                <Image
+                    src="/images/about-image.jpg"
+                    className="rounded-3xl"
+                    width={500}
+                    height={500}
+                    alt="Image"
+                />
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">
-                        About Me
+                        About Us
                     </h2>
                     <p className="text-base lg:text-lg">
-                        We are a dynamic software development company with a
-                        passion for crafting innovative solutions across all
-                        digital landscapes. Our team of skilled professionals
-                        possesses expertise in a wide range of technologies,
-                        allowing us to deliver exceptional results in web
-                        development, mobile app creation, blockchain
-                        integration, and traditional software engineering. From
-                        front-end to back-end, we harness the power of
-                        JavaScript, React, Angular, Vue.js, Node.js, Python,
-                        Java, C++, and more to build interactive and responsive
-                        experiences. Our mobile developers are adept in both
-                        native (Swift, Kotlin) and cross-platform (React Native,
-                        Flutter) frameworks, ensuring your apps reach the widest
-                        audience. We are pioneers in the blockchain space,
-                        leveraging our knowledge of Ethereum, Solidity,
-                        Hyperledger Fabric, and other cutting-edge technologies
-                        to create secure and decentralized applications. Our
-                        commitment to continuous learning and collaboration
-                        drives us to stay ahead of the curve, embracing emerging
-                        technologies and industry best practices. We take pride
-                        in our ability to work seamlessly with clients,
-                        understanding their unique needs and delivering tailored
-                        solutions that exceed expectations. If you are looking
-                        for a trusted partner to bring your software vision to
-                        life, look no further. We are excited to work with you
-                        to create amazing applications that push the boundaries
-                        of what&apos;s possible.
+                        We are a software development company with expertise in
+                        web development, mobile app creation, blockchain
+                        integration, and traditional software engineering. We
+                        use a wide range of technologies to build interactive
+                        and responsive experiences. Our mobile developers are
+                        skilled in both native and cross-platform frameworks. We
+                        are pioneers in the blockchain space, leveraging our
+                        knowledge to create secure and decentralized
+                        applications. We are committed to continuous learning
+                        and collaboration, working seamlessly with clients to
+                        deliver tailored solutions that exceed expectations.
                     </p>
                     <div className="flex flex-row justify-start mt-8">
                         <TabButton
